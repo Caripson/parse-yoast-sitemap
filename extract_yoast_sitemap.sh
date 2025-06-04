dd6dcbe41990defe7f771ada10356be8e99ca2fe
Johan Caripson <31686838+Caripson@users.noreply.github.com>
2025-06-04 08:41:23 +0200

    add shebang and validate arguments

--- extract_yoast_sitemap.sh
+++ extract_yoast_sitemap.sh
@@ -1,22 +1,23 @@
-if [[ $# -eq 0 ]] ; then
-                    echo 'run  extract_yoast_sitemap.sh https://www.yourdomain.com/sitemap_index.xml output.txt'
-                    exit 0
+#!/usr/bin/env bash
+if [[ $# -ne 2 ]]; then
+    echo "Usage: extract_yoast_sitemap.sh <sitemap_index_url> <output_file>"
+    exit 1
 fi
 
 
-echo "# URL list" > $2
+echo "# URL list" > "$2"
 
-main_url=$(curl -s $1 | grep "<loc>" | awk -F"<loc>" '{print $2} ' | awk -F"</loc>" '{print $1}')
+main_url=$(curl -s "$1" | grep "<loc>" | awk -F"<loc>" '{print $2} ' | awk -F"</loc>" '{print $1}')
 
 for i in $main_url
 do
 
-        urls=$(curl -s $i | grep "<loc>" | awk -F"<loc>" '{print $2} ' | awk -F"</loc>" '{print $1}')
+        urls=$(curl -s "$i" | grep "<loc>" | awk -F"<loc>" '{print $2} ' | awk -F"</loc>" '{print $1}')
         for site_url in $urls
         do
 
                 echo "$site_url"
-                echo "$site_url" >> $2
+                echo "$site_url" >> "$2"
 
          done
 
